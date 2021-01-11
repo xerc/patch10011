ec7ef6f3755e9c3852c557f86ee7d00af3c6eddd
Franz Holzinger <holzinger@ttproducts.de>
2021-01-11 18:12:15 +0100

    remove support for old deprecated TypoScript conditions

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,3 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-if (
-    defined('TYPO3_version') &&
-    version_compare(TYPO3_version, '10.0.0', '<')
-) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\Configuration\\TypoScript\\ConditionMatching\\ConditionMatcher'] = [
-        'className' => 'JambageCom\\Patch10011\\Frontend\\Configuration\\TypoScript\\ConditionMatching\\ConditionMatcher'
-    ];
-}
-
