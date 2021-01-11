f16570c4b7ccd818fbd54ff2e81ca166dc77d0fb
Franz Holzinger <holzinger@ttproducts.de>
2021-01-11 18:04:21 +0100

    remove file ext_autoload.php

--- ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('patch10011');
-return array(
-	'JambageCom\\Patch10011\\Frontend\\Configuration\\TypoScript\\ConditionMatching\\ConditionMatcher' => $extensionPath . 'Classes/Frontend/Configuration/TypoScript/ConditionMatching/ConditionMatcher.php',
-	'JambageCom\\Patch10011\\Utility\\ExtensionManagementUtility' => $extensionPath . 'Classes/Utility/ExtensionManagementUtility.php',
-);
-
